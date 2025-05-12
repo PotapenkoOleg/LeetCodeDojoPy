f33f85ffc2ff8f87d7dc7449a7422b5f71a12533
Oleg Potapenko <PotapenkoOleg@gmail.com>
2025-05-12 16:59:01 -0400

    Notes

--- leetcode_dojo/algo/arrays_and_strings/prefix_sum.py
+++ leetcode_dojo/algo/arrays_and_strings/prefix_sum.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def build_prefix_sum(arr):
     prefix = [arr[0]]
     for i in range(1, len(arr)):
@@ -12,3 +15,20 @@ def build_prefix_sum(arr):
 # prefix[j] - prefix[i] + nums[i]
 
 # We MUST initialize our hash map with 0: 1, considering the empty prefix
+# SUM of zero (empty array) appears once
+
+from collections import defaultdict
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        counts = defaultdict(int)
+        counts[0] = 1
+        ans = curr = 0
+
+        for num in nums:
+            curr += num
+            ans += counts[curr - k]
+            counts[curr] += 1
+
+        return ans
